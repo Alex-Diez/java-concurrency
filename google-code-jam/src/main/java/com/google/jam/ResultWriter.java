7ad696e1588eaba86d5cff4768015e349095b90e
Alex-Diez <alexvic.amatory@gmail.com>
2015-07-28 10:23:48 +0300

    add next task's files

--- google-code-jam/src/main/java/com/google/jam/ResultWriter.java
+++ google-code-jam/src/main/java/com/google/jam/ResultWriter.java
@@ -2,22 +2,21 @@ package com.google.jam;
 
 import java.io.IOException;
 import java.io.Writer;
-import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.TreeMap;
 
 public class ResultWriter {
 
 	private final Map<Integer, Integer> resultToWrite;
 
 	public ResultWriter(final Map<Integer, Integer> resultToWrite) {
-		this.resultToWrite = new LinkedHashMap<>(resultToWrite);
+		this.resultToWrite = new TreeMap<>(resultToWrite);
 	}
 
 	public void writeTo(final Writer writer)
 			throws IOException {
-		for (int i = 0; i < resultToWrite.size(); i++) {
-			int value = resultToWrite.get(i + 1);
-			final String formattedOutput = String.format("Case #%d: %d\n", i + 1, value);
+		for (Map.Entry<Integer, Integer> entry : resultToWrite.entrySet()) {
+			final String formattedOutput = String.format("Case #%d: %d\n", entry.getKey(), entry.getValue());
 			writer.write(formattedOutput);
 		}
 		writer.flush();
