cb5701e90b5ffb4d7302bd206dacc4e16a2655bd
Alex-Diez <alexvic.amatory@gmail.com>
2015-08-24 11:32:42 +0300

    Ominous Omino solution delivered

--- google-code-jam/src/main/java/com/google/jam/algorithms/OminousOminoAlgorithm.java
+++ google-code-jam/src/main/java/com/google/jam/algorithms/OminousOminoAlgorithm.java
@@ -2,35 +2,40 @@ package com.google.jam.algorithms;
 
 import java.util.function.Function;
 
+import static java.lang.Integer.parseInt;
+import static java.lang.Math.max;
+import static java.lang.Math.min;
+
 public class OminousOminoAlgorithm
         implements Function<String, String> {
 
     private static final String GABRIEL = "GABRIEL";
-    public static final String RICHARD = "RICHARD";
+    private static final String RICHARD = "RICHARD";
 
     @Override
     public String apply(final String task) {
         final String[] split = task.split("\\s+");
-        final int ominoRate = Integer.parseInt(split[0]);
-        final int length = Integer.parseInt(split[1]);
-        final int width = Integer.parseInt(split[2]);
-        if (length % ominoRate == 0) {
-            if (width % ominoRate == 1) {
-                return RICHARD;
-            }
-            if(width % ominoRate == 0) {
-                return GABRIEL;
-            }
+        final int ominoRate = parseInt(split[0]);
+        final int length = parseInt(split[1]);
+        final int width = parseInt(split[2]);
+        final int large = max(length, width);
+        final int small = min(length, width);
+        if ((large * small) % ominoRate != 0) {
+            return RICHARD;
         }
-        if (width % ominoRate == 0) {
-            if (length % ominoRate == 1) {
-                return RICHARD;
-            }
-            if(length % ominoRate == 0) {
-                return GABRIEL;
-            }
+        if (ominoRate == 3 && small == 1) {
+            return RICHARD;
         }
-        if((length * width) % ominoRate > 0) {
+        if (ominoRate == 4 && small <= 2) {
+            return RICHARD;
+        }
+        if (ominoRate == 5 && (small <= 2 || small == 3 && large == 5)) {
+            return RICHARD;
+        }
+        if (ominoRate == 6 && small <= 3) {
+            return RICHARD;
+        }
+        if (ominoRate > 6) {
             return RICHARD;
         }
         return GABRIEL;
