4cf30692dce07bfb69808534f0a5679c4bc4f198
Alex-Diez <alexvic.amatory@gmail.com>
2015-08-28 15:52:06 +0300

    remove data-structures module due to moving it to another repository

--- data-structures/src/main/java/org/concurrent/datastructures/LastIndexTaskBlockingQueue.java
+++
@@ -1,8 +0,0 @@
-package org.concurrent.datastructures;
-
-import java.util.concurrent.BlockingQueue;
-
-public interface LastIndexTaskBlockingQueue<E>
-        extends BlockingQueue<E>,
-                LastIndexTaskQueue<E> {
-}
--- data-structures/src/main/java/org/concurrent/datastructures/LastIndexTaskQueue.java
+++
@@ -1,9 +0,0 @@
-package org.concurrent.datastructures;
-
-import java.util.Queue;
-
-public interface LastIndexTaskQueue<E>
-        extends Queue<E> {
-
-    int getLastRetrievedTaskIndex();
-}
